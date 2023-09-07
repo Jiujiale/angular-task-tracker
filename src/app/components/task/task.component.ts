6ffb7609d6a226ce4136ffb28a6ece2ed99bad7e
jiaqiu <jiaqiu.win@outlook.com>
2023-09-07 23:52:21 +0800

    init commit && json-server update

---
+++ src/app/components/task/task.component.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core';
+import { Task } from '../../Task';
+import { TASKS } from '../../mock-tasks'
+import { TaskService } from 'src/app/services/task.service';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent {
+  tasks: Task[] = [];
+  taskService: TaskService = inject(TaskService)
+
+  constructor() {
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks
+    })
+  }
+}
