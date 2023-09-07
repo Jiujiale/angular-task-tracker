0343ac15bbbbea6874d0fa2c3664acb119051702
jiaqiu <jiaqiu.win@outlook.com>
2023-09-08 00:27:34 +0800

    deleting tasks

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -17,4 +17,10 @@ export class TaskComponent {
       this.tasks = tasks
     })
   }
+
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe(() => {
+      this.tasks = this.tasks.filter(t => t.id !== task.id)
+    })
+  }
 }
